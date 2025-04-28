fd7f04988f96df60fac04b0c8e760cf86b6b030f
root <root@srv807104.hstgr.cloud>
2025-04-28 09:36:00 +0000

    Initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     const sample = "This is a sample application.";
     return (
-        <div>
+        <div className="text-2xl">
             <h1>React Sample</h1>
             <p>{sample}</p>
         </div>
