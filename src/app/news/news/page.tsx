f6237be261985f84f9e29491dd02f3f9a9ef69d8
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-02 07:19:53 +0000

    Install Sass.

--- src/app/news/news/page.tsx
+++ src/app/news/news/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./style.module.css";
+import styles from "./style.module.scss";
 
 export default function News() {
     return <div className={styles.container}>
@@ -9,8 +9,13 @@ export default function News() {
         </Link>
         <div className={styles.menu}>
             <Link href="#">
-                <Image src="/Logo.svg" alt="씨즈 로고" />
+                <Image src="/Logo.svg" alt="씨즈 로고" width={163} height={51} />
             </Link>
+            <div>
+                <Link href="#" className={styles.sponsor}>
+                    <p>후원하기</p>
+                </Link>
+            </div>
         </div>
     </div>;
 }
