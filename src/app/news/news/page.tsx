1f2ef2828e72c5cb945d7555c7a7c1ec40a662b1
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-02 07:05:08 +0000

    Add Keeper.

--- src/app/news/news/page.tsx
+++ src/app/news/news/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./style.module.css";
+
 export default function News() {
-    return <div></div>;
+    return <div className={styles.container}>
+        <Link href="#" className={styles.keeper}>
+            <p>SEED:S KEEPER</p>
+        </Link>
+        <div className={styles.menu}>
+            <Link href="#">
+                <Image src="/Logo.svg" alt="씨즈 로고" />
+            </Link>
+        </div>
+    </div>;
 }
