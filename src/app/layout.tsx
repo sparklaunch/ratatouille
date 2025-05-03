27376e88c04911ea25fce4a54bbf8934a13be844
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-03 00:28:16 +0000

    Convert <link> to @import;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,14 +13,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ko">
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet" />
-            </Head>
-            <body style={{
-                fontFamily: "Nanum Gothic"
-            }}>
+            <body>
                 {children}
             </body>
         </html>
