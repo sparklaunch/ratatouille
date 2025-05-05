7b3d78194b563b84471a924902bb437d9745df19
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-05 01:17:30 +0000

    Modify route.ts

--- src/app/api/notices/route.ts
+++ src/app/api/notices/route.ts
@@ -1,18 +1,19 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 
-export async function GET(req: Request) {
-    const { searchParams } = new URL(req.url);
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
     const token = searchParams.get("token");
-
     if (token !== process.env.API_SECRET_KEY) {
-        return new NextResponse("Unauthorized", { status: 401 });
+        return new NextResponse("Unauthorized", {
+            status: 401
+        });
     }
-
     const notices = await prisma.notices.findMany({
-        orderBy: { created_at: "desc" },
+        orderBy: {
+            created_at: "desc"
+        },
         take: 100,
     });
-
     return NextResponse.json(notices);
 }
