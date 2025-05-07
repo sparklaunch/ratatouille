882803fb5c7ecf48eefec3ead9b5fa02b649b734
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-07 09:40:14 +0000

    Modify header.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,55 +7,57 @@ export default function Header() {
         <Link href="#" className={styles.keeper}>
             <span>SEED:S KEEPER</span>
         </Link>
-        <div className={styles.menu}>
-            <Link href="/">
-                <Image src="/logos/Logo.svg" alt="씨즈 로고" width={163} height={51} />
-            </Link>
-            <div className={styles.navigationContainer}>
-                <div className={styles.navigation}>
-                    <Link href="#" className={styles.menuItem}>
-                        <span>브랜드 스토리</span>
-                    </Link>
-                    <Link href="#" className={styles.menuItem}>
-                        <span>프로젝트</span>
-                    </Link>
-                    <div className={styles.subMenuContainer}>
-                        <Link href="#" className={styles.menuItem}>
-                            <span>소식</span>
-                        </Link>
-                        <div className={styles.subMenuOuterContainer}>
-                            <div className={styles.subMenu}>
-                                <Link href="/news/notice" className={styles.subMenuItem}>
-                                    <span>공지 사항</span>
-                                </Link>
-                                <Link href="#" className={styles.subMenuItem}>
-                                    <span>언론 보도</span>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.subMenuContainer}>
-                        <Link href="#" className={styles.menuItem}>
-                            <span>소통</span>
-                        </Link>
-                        <div className={styles.subMenuOuterContainer}>
-                            <div className={styles.subMenu}>
-                                <Link href="#" className={styles.subMenuItem}>
-                                    <span>뉴스레터</span>
-                                </Link>
-                                <Link href="#" className={styles.subMenuItem}>
-                                    <span>찾아오시는 길</span>
-                                </Link>
-                                <Link href="#" className={styles.subMenuItem}>
-                                    <span>문의하기</span>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Link href="#" className={styles.sponsor}>
-                    <span>후원하기</span>
+        <div className={styles.menuContainer}>
+            <div className={styles.menu}>
+                <Link href="/">
+                    <Image src="/logos/Logo.svg" alt="씨즈 로고" width={161} height={50} />
                 </Link>
+                <div className={styles.navigationContainer}>
+                    <div className={styles.navigation}>
+                        <Link href="#" className={styles.menuItem}>
+                            <span>브랜드 스토리</span>
+                        </Link>
+                        <Link href="#" className={styles.menuItem}>
+                            <span>프로젝트</span>
+                        </Link>
+                        <div className={styles.subMenuContainer}>
+                            <Link href="#" className={styles.menuItem}>
+                                <span>소식</span>
+                            </Link>
+                            <div className={styles.subMenuOuterContainer}>
+                                <div className={styles.subMenu}>
+                                    <Link href="/news/notice" className={styles.subMenuItem}>
+                                        <span>공지 사항</span>
+                                    </Link>
+                                    <Link href="#" className={styles.subMenuItem}>
+                                        <span>언론 보도</span>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.subMenuContainer}>
+                            <Link href="#" className={styles.menuItem}>
+                                <span>소통</span>
+                            </Link>
+                            <div className={styles.subMenuOuterContainer}>
+                                <div className={styles.subMenu}>
+                                    <Link href="#" className={styles.subMenuItem}>
+                                        <span>뉴스레터</span>
+                                    </Link>
+                                    <Link href="#" className={styles.subMenuItem}>
+                                        <span>찾아오시는 길</span>
+                                    </Link>
+                                    <Link href="#" className={styles.subMenuItem}>
+                                        <span>문의하기</span>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Link href="#" className={styles.sponsor}>
+                        <span>후원하기</span>
+                    </Link>
+                </div>
             </div>
         </div>
     </div>;
