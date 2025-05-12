9f94e766ede974cf57974f5d139ec7b9c6b093a8
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-12 04:37:27 +0000

    Add MUI Accordion.

--- src/app/brand/introduction/page.tsx
+++ src/app/brand/introduction/page.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
+import { ExpandMore } from "@mui/icons-material";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import Link from "next/link";
 import styles from "./style.module.scss";
 
@@ -17,7 +19,23 @@ export default function IntroductionPage() {
                 </Link>
             </div>
             <div className={styles.historyContainer}>
-
+                <h2 className={styles.historyHeader}>History</h2>
+                <Accordion sx={{
+                    boxShadow: "none",
+                    borderTop: "1px solid black"
+                }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                        sx={{
+                            height: 80
+                        }}
+                    >
+                        <h3 className={styles.accordionHeader}><span className={styles.accordionHeaderTitle}>SEASON 3</span> (2020~현재) 청년 정책 사각 지대 해소와 사회적 경제 커먼즈 구축</h3>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        This is content.
+                    </AccordionDetails>
+                </Accordion>
             </div>
         </div>
         <Footer />
