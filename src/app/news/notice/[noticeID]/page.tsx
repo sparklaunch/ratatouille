8197c1205e073ae838ee174cc3131aed786274da
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-13 04:18:59 +0000

    Create notice content page header.

--- src/app/news/notice/[noticeID]/page.tsx
+++ src/app/news/notice/[noticeID]/page.tsx
@@ -1,5 +1,22 @@
-export default function NoticeContentPage({ params }: { params: { noticeID: String } }) {
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Link from "next/link";
+import styles from "./style.module.scss";
+
+export default async function NoticeContentPage({ }: { params: { noticeID: String } }) {
     return <>
-        <p>{params.noticeID}</p>
+        <Header />
+        <div className={styles.container}>
+            <h1 className={styles.header}>소식</h1>
+            <div className={styles.subheader}>
+                <Link href="/news/notice">
+                    <span className={styles.activeSubheader}>공지 사항</span>
+                </Link>
+                <Link href="/news/press">
+                    <span>언론 보도</span>
+                </Link>
+            </div>
+        </div>
+        <Footer />
     </>;
 }
