86bf27151fadb19b6f0b15b494bbd1c29e45c493
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-14 03:21:28 +0000

    Create press page.

---
+++ src/app/news/press/page.tsx
@@ -0,0 +1,22 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Link from "next/link";
+import styles from "./style.module.scss";
+
+export default async function PressPage() {
+    return <>
+        <Header />
+        <div className={styles.container}>
+            <h1 className={styles.header}>소식</h1>
+            <div className={styles.subheader}>
+                <Link href="/news/notice">
+                    <span>공지 사항</span>
+                </Link>
+                <Link href="/news/press">
+                    <span className={styles.activeSubheader}>언론 보도</span>
+                </Link>
+            </div>
+        </div>
+        <Footer />
+    </>;
+}
