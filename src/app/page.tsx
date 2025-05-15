ee71980df689874166a79286b8f35a2e8f3e8c48
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-15 09:53:52 +0000

    Add video.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
+import styles from "./style.module.scss";
 
 export default function Home() {
     return (
         <>
             <Header />
+            <div className={styles.videoContainer}>
+                <video autoPlay loop muted playsInline className={styles.video}>
+                    <source src="/videos/Video.mp4" type="video/mp4" />
+                </video>
+            </div>
             <Footer />
         </>
     );
