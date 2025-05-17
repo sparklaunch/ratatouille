dd87fdf6b14f65f435e88b100e79a05eec32c97b
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-17 02:07:08 +0000

    Create shared input fields.

--- src/app/communication/inquiry/InquiryForm.tsx
+++ src/app/communication/inquiry/InquiryForm.tsx
@@ -1,30 +1,61 @@
 "use client";
 
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import Form from "next/form";
 import Image from "next/image";
 import { useState } from "react";
 import styles from "./style.module.scss";
 
+const sharedFontSettings = {
+    fontFamily: "Nanum Gothic",
+    fontSize: "17px"
+}
+
+const textFieldSlotProps = {
+    inputLabel: {
+        shrink: true,
+        sx: {
+            sharedFontSettings,
+            fontWeight: "bold"
+        }
+    },
+    htmlInput: {
+        sx: sharedFontSettings
+    }
+}
+
 export default function InquiryForm() {
-    const [type] = useState("inquiry");
+    const [type, setType] = useState("inquiry");
+    const [name, setName] = useState("");
     return <div className={styles.inquiryContainer}>
         <Image src="/images/Inquiry.png" alt="문의하기 이미지" fill className={styles.inquiryImage} />
         <Form action="/api/inquiry">
-            <FormControl fullWidth>
+            <FormControl fullWidth sx={{ marginBottom: "20px" }}>
                 <InputLabel id="type" sx={{
-                    fontFamily: "Nanum Gothic",
-                    fontWeight: "bold",
-                    fontSize: 17
+                    sharedFontSettings,
+                    fontWeight: "bold"
                 }}>선택</InputLabel>
-                <Select labelId="type" label="선택" value={type} MenuProps={{ disableScrollLock: true }} sx={{
-                    fontFamily: "Nanum Gothic",
-                    fontSize: 17
-                }}>
-                    <MenuItem value="inquiry">문의하기</MenuItem>
-                    <MenuItem value="visit">두더집 탐방 신청하기</MenuItem>
+                <Select labelId="type" label="선택" value={type} MenuProps={{ disableScrollLock: true }} sx={sharedFontSettings} onChange={(event) => setType(event.target.value)}>
+                    <MenuItem value="inquiry" sx={sharedFontSettings}>문의하기</MenuItem>
+                    <MenuItem value="visit" sx={sharedFontSettings}>두더집 탐방 신청하기</MenuItem>
                 </Select>
             </FormControl>
+            <div className={styles.inquiryGridContainer}>
+                <FormControl fullWidth>
+                    <TextField label="성명" variant="outlined" placeholder="이름을 입력해주세요" slotProps={textFieldSlotProps} value={name} onChange={(event) => setName(event.target.value)} />
+                </FormControl>
+                <FormControl fullWidth>
+                    <TextField label="소속" variant="outlined" placeholder="소속팀" slotProps={textFieldSlotProps} />
+                </FormControl>
+            </div>
+            <div className={styles.inquiryGridContainer}>
+                <FormControl fullWidth>
+                    <TextField label="연락처" variant="outlined" placeholder="000-000-0000" type="tel" slotProps={textFieldSlotProps} />
+                </FormControl>
+                <FormControl fullWidth>
+                    <TextField label="이메일" variant="outlined" placeholder="example@email.com" type="email" slotProps={textFieldSlotProps} />
+                </FormControl>
+            </div>
         </Form>
     </div >;
 }
