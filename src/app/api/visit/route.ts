0c32d3f8ceec7d0084460d70a821b3c19e77942f
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-20 03:32:10 +0000

    Add Zod validation to visits.

--- src/app/api/visit/route.ts
+++ src/app/api/visit/route.ts
@@ -1,80 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 import { prisma } from "../../../../lib/prisma";
 
+const visitSchema = z.object({
+    name: z.string().trim().min(1).max(100),
+    affiliation: z.string().max(100),
+    contact: z.string().trim().regex(/^\d{2,3}-\d{3,4}-\d{4}$/),
+    email: z.string().trim().regex(/^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/),
+    applicationDate: z.coerce.date(),
+    visitDateTime: z.coerce.date(),
+    headCount: z.number().min(1).max(100),
+    purposeOfFieldTrip: z.boolean(),
+    purposeOfListening: z.boolean(),
+    other: z.string(),
+    termsAgreed: z.literal(true)
+});
+
 export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const { name, contact, email, applicationDate, visitDateTime, headCount, purposeOfFieldTrip, purposeOfListening, termsAgreed } = body;
-    if (!name.trim()) {
+    try {
+        const body = await request.json();
+        visitSchema.parse(body);
+        await prisma.visits.create({
+            data: body
+        });
+        return NextResponse.json({
+            hasSucceeded: true,
+            message: "Visit accepted"
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({
+                hasSucceeded: false,
+                message: error.message
+            });
+        }
         return NextResponse.json({
             hasSucceeded: false,
-            message: "Name is required"
+            message: String(error)
         });
     }
-    if (!contact) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Contact is required"
-        });
-    }
-    if (contact.length < 11) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Contact is invalid"
-        });
-    }
-    if (!email) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Email is required"
-        });
-    }
-    if (!/^[a-z0-9._%+-]{1,}@[a-z0-9-]+(\.[a-z0-9-]+)*\.[a-z]{2,}$/.test(email)) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Email is invalid"
-        });
-    }
-    if (!applicationDate) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Application date is required"
-        });
-    }
-    if (!visitDateTime) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Visit datetime is required"
-        });
-    }
-    if (headCount <= 0) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Head count cannot be fewer than 1"
-        });
-    }
-    if (!purposeOfFieldTrip) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Purpose of field trip required"
-        });
-    }
-    if (!purposeOfListening) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Purpose of listening is required"
-        });
-    }
-    if (!termsAgreed) {
-        return NextResponse.json({
-            hasSucceeded: false,
-            message: "Terms are not agreed"
-        });
-    }
-    await prisma.visits.create({
-        data: body
-    });
-    return NextResponse.json({
-        hasSucceeded: true,
-        message: "Visit accepted"
-    });
 }
