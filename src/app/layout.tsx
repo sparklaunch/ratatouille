bd7e9b7fd6d284712332cd82671378124d68101f
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-20 10:57:47 +0000

    Enrich metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,13 @@ import "./globals.css";
 
 export const metadata: Metadata = {
     title: "씨즈",
-    description: "씨즈 웹사이트"
+    description: "씨즈 웹사이트",
+    openGraph: {
+        type: "website",
+        title: "씨즈",
+        description: "씨즈 웹사이트",
+        siteName: "씨즈"
+    }
 };
 
 export default async function RootLayout({
