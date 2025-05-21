622d1dbe70e4c9f7cdcda0bbcdd6733524296df3
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-21 05:04:11 +0000

    Complete Korean inquiry header.

--- src/app/[locale]/communication/inquiry/page.tsx
+++ src/app/[locale]/communication/inquiry/page.tsx
@@ -1,23 +1,25 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import { Link } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
 import InquiryForm from "./InquiryForm";
 import styles from "./style.module.scss";
 
 export default async function InquiryPage() {
+    const t = await getTranslations("inquiry");
     return <>
         <Header />
         <div className={styles.container}>
-            <h1 className={styles.header}>소통</h1>
+            <h1 className={styles.header}>{t("communication")}</h1>
             <div className={styles.subheader}>
                 <Link href="/communication/newsletter">
-                    <span>뉴스레터</span>
+                    <span>{t("news-letter")}</span>
                 </Link>
                 <Link href="/communication/direction">
-                    <span>찾아오시는 길</span>
+                    <span>{t("directions")}</span>
                 </Link>
                 <Link href="/communication/inquiry" className={styles.activeSubheader}>
-                    <span>문의하기</span>
+                    <span>{t("inquiry")}</span>
                 </Link>
             </div>
         </div>
