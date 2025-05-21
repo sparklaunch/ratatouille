f7b615d24477b0c9e2faf4a81456ab5607e792a4
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-21 05:36:09 +0000

    Complete Korean inquiry form.

--- src/app/[locale]/communication/inquiry/InquiryForm.tsx
+++ src/app/[locale]/communication/inquiry/InquiryForm.tsx
@@ -152,48 +152,48 @@ export default function InquiryForm() {
         }
     };
     return <div className={styles.inquiryContainer}>
-        <Image src="/images/Inquiry.png" alt="문의하기 이미지" fill className={styles.inquiryImage} />
+        <Image src="/images/Inquiry.png" alt={t("inquiry-alternate")} fill className={styles.inquiryImage} />
         <form onSubmit={handleSubmit} className={styles.inquiryForm}>
             <FormControl fullWidth sx={{ marginBottom: "20px" }}>
-                <InputLabel id="type">선택</InputLabel>
-                <Select labelId="type" label="선택" value={type} MenuProps={{ disableScrollLock: true }} onChange={(event) => setType(event.target.value)} >
-                    <MenuItem value={InquiryType.Inquiry} sx={menuItemSharedStyles}>문의하기</MenuItem>
-                    <MenuItem value={InquiryType.Visit} sx={menuItemSharedStyles}>두더집 탐방 신청하기</MenuItem>
+                <InputLabel id="type">{t("type")}</InputLabel>
+                <Select labelId="type" label={t("type")} value={type} MenuProps={{ disableScrollLock: true }} onChange={(event) => setType(event.target.value)} >
+                    <MenuItem value={InquiryType.Inquiry} sx={menuItemSharedStyles}>{t("inquire")}</MenuItem>
+                    <MenuItem value={InquiryType.Visit} sx={menuItemSharedStyles}>{t("visit")}</MenuItem>
                 </Select>
             </FormControl>
             <div className={styles.inquiryGridContainer}>
                 <FormControl fullWidth>
-                    <TextField label="성명" variant="outlined" placeholder="이름을 입력해주세요" value={name} onChange={(event) => {
+                    <TextField label={t("name")} variant="outlined" placeholder={t("name-placeholder")} value={name} onChange={(event) => {
                         const input = event.target.value;
                         const name = input.replace(/[^ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-zA-ZÀ-ž|\-|'|\s]/g, "");
                         setName(name);
-                    }} slotProps={textFieldSharedSlotProps} required title="이름을 입력해주세요" />
+                    }} slotProps={textFieldSharedSlotProps} required title={t("name-placeholder")} />
                 </FormControl>
                 <FormControl fullWidth>
-                    <TextField label="소속" variant="outlined" placeholder="소속팀" value={affiliation} onChange={(event) => setAffiliation(event.target.value)} slotProps={textFieldSharedSlotProps} />
+                    <TextField label={t("affiliation")} variant="outlined" placeholder={t("affiliation-placeholder")} value={affiliation} onChange={(event) => setAffiliation(event.target.value)} slotProps={textFieldSharedSlotProps} />
                 </FormControl>
             </div>
             <div className={styles.inquiryGridContainer}>
                 <FormControl fullWidth>
-                    <TextField label="연락처" variant="outlined" placeholder="000-000-0000" type="tel" value={contact} onChange={(event) => {
+                    <TextField label={t("contact")} variant="outlined" placeholder={t("contact-example")} type="tel" value={contact} onChange={(event) => {
                         const input = event.target.value;
                         const contact = formatPhoneNumber(input);
                         setContact(contact);
-                    }} slotProps={textFieldSharedSlotProps} required title="연락처를 입력해주세요" />
+                    }} slotProps={textFieldSharedSlotProps} required title={t("contact-placeholder")} />
                 </FormControl>
                 <FormControl fullWidth>
-                    <TextField label="이메일" variant="outlined" placeholder="example@email.com" value={email} onChange={(event) => {
+                    <TextField label={t("email")} variant="outlined" placeholder={t("email-example")} value={email} onChange={(event) => {
                         const input = event.target.value;
                         const email = input.replace(/\s/g, "");
                         setEmail(email);
-                    }} slotProps={textFieldSharedSlotProps} required title="이메일을 입력해주세요" />
+                    }} slotProps={textFieldSharedSlotProps} required title={t("email-placeholder")} />
                 </FormControl>
             </div>
             {type === InquiryType.Visit && <>
                 <div className={styles.inquiryGridContainer}>
                     <FormControl fullWidth>
                         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-                            <DatePicker label="신청 날짜" value={applicationDate} onChange={(date) => setApplicationDate(date)} sx={{
+                            <DatePicker label={t("application-date")} value={applicationDate} onChange={(date) => setApplicationDate(date)} sx={{
                                 ".MuiPickersSectionList-sectionContent": {
                                     fontFamily: "Nanum Gothic",
                                     fontSize: "17px"
@@ -203,7 +203,7 @@ export default function InquiryForm() {
                     </FormControl>
                     <FormControl fullWidth>
                         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-                            <DateTimePicker label="방문 일시" value={visitDateTime} onChange={(dateTime) => setVisitDateTime(dateTime)} slotProps={{
+                            <DateTimePicker label={t("visit-datetime")} value={visitDateTime} onChange={(dateTime) => setVisitDateTime(dateTime)} slotProps={{
                                 textField: {
                                     InputLabelProps: {
                                         shrink: true
@@ -219,7 +219,7 @@ export default function InquiryForm() {
                     </FormControl>
                 </div>
                 <FormControl fullWidth sx={{ marginBottom: "20px" }}>
-                    <TextField label="방문 인원" variant="outlined" placeholder="신청인 外 00명 (소속을 달리할 경우 소속별 인원 기재)" value={headCount} onChange={(event) => {
+                    <TextField label={t("head-count")} variant="outlined" placeholder={t("head-count-example")} value={headCount} onChange={(event) => {
                         const input = event.target.value;
                         if (!input) {
                             setHeadCount(0);
@@ -227,18 +227,18 @@ export default function InquiryForm() {
                             const headCount = parseInt(input.replace(/\D/g, ""));
                             setHeadCount(headCount);
                         }
-                    }} slotProps={textFieldSharedSlotProps} required title="방문 인원을 입력해주세요" />
+                    }} slotProps={textFieldSharedSlotProps} required title={t("head-count-placeholder")} />
                 </FormControl>
                 <div className={styles.purposeOuterContainer}>
-                    <p>방문 목적</p>
+                    <p>{t("visit-purpose")}</p>
                     <div className={styles.purposeInnerContainer}>
-                        <p>희망 시 체크</p>
+                        <p>{t("check-if-necessary")}</p>
                         <FormControlLabel control={<Checkbox sx={{
                             color: "#FF301E",
                             "&.Mui-checked": {
                                 color: "#FF301E"
                             }
-                        }} checked={purposeOfFieldTrip} onChange={(event) => setPurposeOfFieldTrip(event.target.checked)} />} label="두더집 탐방 (시설 및 운영, 두더지땅굴 소개 등)" slotProps={{
+                        }} checked={purposeOfFieldTrip} onChange={(event) => setPurposeOfFieldTrip(event.target.checked)} />} label={t("visit-checkbox")} slotProps={{
                             typography: {
                                 fontFamily: "Nanum Gothic",
                                 fontSize: 17
@@ -249,7 +249,7 @@ export default function InquiryForm() {
                             "&.Mui-checked": {
                                 color: "#FF301E"
                             }
-                        }} checked={purposeOfListening} onChange={(event) => setPurposeOfListening(event.target.checked)} />} label="고립·은둔 청년 사업 관련 설명 청취" slotProps={{
+                        }} checked={purposeOfListening} onChange={(event) => setPurposeOfListening(event.target.checked)} />} label={t("listening-checkbox")} slotProps={{
                             typography: {
                                 fontFamily: "Nanum Gothic",
                                 fontSize: 17
@@ -259,20 +259,20 @@ export default function InquiryForm() {
                 </div>
             </>}
             <FormControl fullWidth sx={{ marginBottom: "20px" }}>
-                <TextField label="기타 사항" variant="outlined" placeholder="기타 사항을 입력하세요" multiline rows="5" value={other} onChange={(event) => setOther(event.target.value)} slotProps={textFieldSharedSlotProps} />
+                <TextField label={t("other")} variant="outlined" placeholder={t("other-placeholder")} multiline rows="5" value={other} onChange={(event) => setOther(event.target.value)} slotProps={textFieldSharedSlotProps} />
             </FormControl>
             {type === InquiryType.Visit && <div className={styles.informationContainer}>
-                <p>탐방비 안내</p>
+                <p>{t("fee-information")}</p>
                 <ul>
-                    <li>- 1시간 기준: 200,000원, 1시간 ~ 2시간: 300,000원
+                    <li>{t("fee-title")}
                         <ul className={styles.informationList}>
-                            <li>• 방문 기관 사정에 따라 강사비(지급 기준 적용)로 지급 가능하며, 기타 비용으로 납부 시, 기부금 영수증 발행 가능. (신한은행 100-026-478197 (사)씨즈)</li>
-                            <li>• 탐방비는 고립·은둔 청년들을 위한 공간 및 프로그램 운영비로 사용됩니다.</li>
+                            <li>{t("fee-content-0")}</li>
+                            <li>{t("fee-content-1")}</li>
                         </ul>
                     </li>
-                    <li>- 운영 시간 안내
+                    <li>{t("hours-title")}
                         <ul className={styles.informationList}>
-                            <li>• 수 ~ 토: 12:00 ~ 20:00</li>
+                            <li>{t("hours-content")}</li>
                         </ul>
                     </li>
                 </ul>
@@ -282,13 +282,13 @@ export default function InquiryForm() {
                 "&.Mui-checked": {
                     color: "#FF301E"
                 }
-            }} checked={termsAgreed} onChange={(event) => setTermsAgreed(event.target.checked)} />} label="이용 약관 / 개인 정보 수집 및 이용 동의" slotProps={{
+            }} checked={termsAgreed} onChange={(event) => setTermsAgreed(event.target.checked)} />} label={t("terms-checkbox")} slotProps={{
                 typography: {
                     fontFamily: "Nanum Gothic",
                     fontSize: 15
                 }
             }} />
-            <button className={styles.submitButton} type="submit">보내기</button>
+            <button className={styles.submitButton} type="submit">{t("submit")}</button>
         </form>
     </div >;
 }
