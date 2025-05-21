92025bcadb9a0b59b2cde56286e26c37e138d42a
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-21 04:41:17 +0000

    Complete Korean articles page.

--- src/app/[locale]/news/press/page.tsx
+++ src/app/[locale]/news/press/page.tsx
@@ -2,21 +2,23 @@ import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import { Link } from "@/i18n/routing";
 import { Skeleton } from "@mui/material";
+import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 import ArticleList from "./ArticleList";
 import styles from "./style.module.scss";
 
 export default async function PressPage() {
+    const t = await getTranslations("press");
     return <>
         <Header />
         <div className={styles.container}>
-            <h1 className={styles.header}>소식</h1>
+            <h1 className={styles.header}>{t("news")}</h1>
             <div className={styles.subheader}>
                 <Link href="/news/notice">
-                    <span>공지 사항</span>
+                    <span>{t("notice")}</span>
                 </Link>
                 <Link href="/news/press">
-                    <span className={styles.activeSubheader}>언론 보도</span>
+                    <span className={styles.activeSubheader}>{t("press-release")}</span>
                 </Link>
             </div>
             <Suspense fallback={<Skeleton />}>
