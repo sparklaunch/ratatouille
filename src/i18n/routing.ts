7c6b0735d82f9b1941d58dc088f2eacf330cdd3f
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-22 06:40:21 +0000

    Add Spanish.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    locales: ["ko", "en", "ja"],
+    locales: ["ko", "en", "ja", "es"],
     defaultLocale: "ko",
     localePrefix: "as-needed"
 });
