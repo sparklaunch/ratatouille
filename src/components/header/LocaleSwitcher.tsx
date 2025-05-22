2f3810b77eaa22d151074346e72f9037b11b30bf
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-22 05:34:51 +0000

    Fix one, remaining one.

--- src/components/header/LocaleSwitcher.tsx
+++ src/components/header/LocaleSwitcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { routing, usePathname, useRouter } from "@/i18n/routing";
 import localeMapper from "@/utilities/localeMapper.json";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
@@ -8,9 +9,20 @@ import Image from "next/image";
 import { useState } from "react";
 import styles from "./style.module.scss";
 
-const availableLocales: Locale[] = ["ko", "en", "ja"];
-
 export default function LocaleSwitcher() {
+    const router = useRouter();
+    const pathname = usePathname();
+    function changeLocale(nextLocale: Locale) {
+        let basePath = pathname;
+        for (const localeItem of routing.locales) {
+            if (basePath === `/${localeItem}`) {
+                basePath = "/";
+            } else if (basePath.startsWith(`/${localeItem}/`)) {
+                basePath = basePath.replace(`/${localeItem}`, "");
+            }
+        }
+        router.push(basePath || "/", { locale: nextLocale });
+    }
     const [isOpen, setIsOpen] = useState(false);
     const locale = useLocale();
     const currentLocale = localeMapper[locale];
@@ -20,7 +32,7 @@ export default function LocaleSwitcher() {
             {isOpen ? <ArrowDropDownIcon style={{ color: "#FF301E" }} fontSize="large" /> : <ArrowDropUpIcon style={{ color: "#FF301E" }} fontSize="large" />}
         </div>
         {isOpen && <div className={styles.panelContainer}>
-            {availableLocales.map(availableLocale => <div className={styles.locale} key={availableLocale}>
+            {routing.locales.map(availableLocale => <div className={styles.locale} key={availableLocale} onClick={() => changeLocale(availableLocale)}>
                 <Image src={`/flags/${availableLocale}.svg`} alt={localeMapper[availableLocale].flag} width={30} height={20} className={styles.flagImage} />
                 <p>{localeMapper[availableLocale].language}</p>
             </div>)}
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,10 @@
-import { Locale } from "next-intl";
+import { hasLocale, Locale } from "next-intl";
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-    let locale = await requestLocale as Locale;
-    if (!locale || !routing.locales.includes(locale)) {
-        locale = routing.defaultLocale;
-    }
+    let requestedLocale = await requestLocale as Locale;
+    const locale = hasLocale(routing.locales, requestedLocale) ? requestedLocale : routing.defaultLocale;
     return {
         locale,
         messages: (await import(`../../messages/${locale}.json`)).default
