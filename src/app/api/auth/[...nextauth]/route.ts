f9a6105099460074ca9f41804a03414c71c70744
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-24 09:05:37 +0000

    Separate authOptions.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,5 @@
-import bcrypt from "bcrypt";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { prisma } from "../../../../../lib/prisma";
-
-export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: {
-                    label: "Email",
-                    type: "email"
-                },
-                password: {
-                    label: "Password",
-                    type: "password"
-                }
-            },
-            async authorize(credentials) {
-                try {
-                    const administrator = await prisma.administrators.findFirst({
-                        where: {
-                            email: credentials?.email
-                        }
-                    });
-                    const isPasswordCorrect = await bcrypt.compare(credentials!.password, administrator!.password);
-                    if (!isPasswordCorrect) {
-                        return null;
-                    }
-                    return administrator;
-                } catch (error) {
-                    throw error;
-                }
-            }
-        })
-    ],
-    session: {
-        strategy: "jwt"
-    }
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 
