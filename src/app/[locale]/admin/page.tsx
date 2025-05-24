afe32f55ce6521455935ad4628d5685d3ba0a477
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-24 03:51:05 +0000

    Complete admin page.

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,5 +1,46 @@
+"use client";
+
+import { useRouter } from "@/i18n/routing";
 import styles from "./style.module.scss";
 
-export default async function AdminPage() {
-    return <div className={styles.container}></div>;
+export default function AdminPage() {
+    const router = useRouter();
+    const signIn = async (formData: FormData) => {
+        const username = formData.get("username");
+        const password = formData.get("password");
+        try {
+            const response = await fetch("/api/admin/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            });
+            if (response.ok) {
+                router.push("/admin/main");
+            } else {
+                alert("Request failed");
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                alert(error.message);
+            } else {
+                alert(String(error));
+            }
+        }
+    };
+    return <div className={styles.container}>
+        <div className={styles.formContainer}>
+            <form className={styles.form} action={signIn}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username" />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" />
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+    </div>;
 }
