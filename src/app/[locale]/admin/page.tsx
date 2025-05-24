762e468bb981ecd5c4518c1b473dfad553c3b61d
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-24 06:29:40 +0000

    Fix auth API logic.

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,15 +1,34 @@
 "use client";
 
+import { useRouter } from "@/i18n/routing";
+import { signIn } from "next-auth/react";
+import { FormEvent, useState } from "react";
 import styles from "./style.module.scss";
 
 export default function AdminPage() {
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        const response = await signIn("credentials", {
+            redirect: false,
+            email,
+            password
+        });
+        if (response?.ok) {
+            router.push("/admin/main");
+        } else {
+            alert("Failed to sign in");
+        }
+    };
     return <div className={styles.container}>
         <div className={styles.formContainer}>
-            <form className={styles.form} >
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} />
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
+                <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} />
                 <button type="submit">Sign In</button>
             </form>
         </div>
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,45 @@
+import bcrypt from "bcrypt";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "../../../../../lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email"
+                },
+                password: {
+                    label: "Password",
+                    type: "password"
+                }
+            },
+            async authorize(credentials) {
+                try {
+                    const administrator = await prisma.administrators.findFirst({
+                        where: {
+                            email: credentials?.email
+                        }
+                    });
+                    const isPasswordCorrect = await bcrypt.compare(credentials!.password, administrator!.password);
+                    if (!isPasswordCorrect) {
+                        return null;
+                    }
+                    return administrator;
+                } catch (error) {
+                    throw error;
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt"
+    }
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
