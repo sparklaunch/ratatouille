98ea8ea2b7570e8496b1e2c7225e7e63a10b5e66
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-24 11:07:51 +0000

    Fix newsletter page.

--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../../../lib/prisma";
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
-    const id = parseInt(searchParams.get("id") as string);
+    const id = searchParams.get("id") as string;
     const newsLetter = await prisma.newsletters.findUnique({
         where: {
             id
