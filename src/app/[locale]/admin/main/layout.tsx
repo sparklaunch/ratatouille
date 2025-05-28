78731b491c34bf95043cb756ce8f198e6de2ab35
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-05-28 04:17:18 +0000

    Complete permission denied page.

--- src/app/[locale]/admin/main/layout.tsx
+++ src/app/[locale]/admin/main/layout.tsx
@@ -1,11 +1,12 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import PermissionDeniedPage from "@/components/permissionDeniedPage/PermissionDeniedPage";
 import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
 
 export default async function AdminMainLayout({ children }: { children: ReactNode }) {
     const session = await getServerSession(authOptions);
     if (!session) {
-        return <p>Permission denied</p>;
+        return <PermissionDeniedPage />;
     }
     return children;
 }
---
+++ src/components/permissionDeniedPage/PermissionDeniedPage.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./style.module.scss";
+
+export default async function PermissionDeniedPage() {
+    return <div className={styles.container}>
+        <Image src="/images/NoWalking.png" alt="접근 거부 이미지" fill className={styles.noWalkingImage} />
+        <h1 className={styles.header}>접근 거부</h1>
+        <p className={styles.paragraph}>관리자 페이지에 접속하려면 먼저 로그인하세요</p>
+    </div>
+}
