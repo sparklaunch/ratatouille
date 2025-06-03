fc37806ddcb80eb42a98c105e107d47afd9962c0
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-06-03 01:21:41 +0000

    Add prelude background.

--- src/app/[locale]/donation/prelude/page.tsx
+++ src/app/[locale]/donation/prelude/page.tsx
@@ -12,7 +12,14 @@ export default async function PreludePage() {
             <Link href="/">
                 <Image src="/logos/Logo.svg" alt="Seeds logo" fill className={styles.logo} />
             </Link>
-            <h2>누군가의 '혼자가 아니야'라는 한마디가, 당신을 살린 적이 있나요?</h2>
+            <h2>누군가의 '혼자가 아니야'라는 한마디가,<br />당신을 살린 적이 있나요?</h2>
+        </div>
+        <div className={styles.preludeBackgroundContainer}>
+            <Image src="/images/PreludeBackground.png" alt="Prelude background image" fill />
+            <div className={styles.preludeBackgroundContent}>
+                <h3>그 한마디로 버텨낸 시간.<br />그 따뜻한 눈빛으로 다시 일어선 날.<br />우리 모두는 누군가의 온기로 살아왔습니다.<br />이제, 당신이 그 온기가 되어주세요.<br />단절된 청년들에게 다시 연결될 수 있는 희망을 건네는 일.<br />씨즈와 함께라면 가능합니다.</h3>
+                <Image src="/images/PreludeForeground.png" alt="Prelude foreground image" fill />
+            </div>
         </div>
     </>;
 }
