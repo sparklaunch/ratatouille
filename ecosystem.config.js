bdd063a730fe35065ac7e9a6ca0116cd26f924f5
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-06-06 11:20:24 +0900

    Adjust clean-up schedule.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-    apps: [
-        {
-            name: "clean-up",
-            script: "npm",
-            args: "run clean-up",
-            cron_restart: "0 0 * * *",
-            autorestart: false,
-            watch: false
-        }
-    ]
-};
+export const apps = [
+    {
+        name: "clean-up",
+        script: "npm",
+        args: "run clean-up",
+        cron_restart: "0 15 * * *",
+        autorestart: false,
+        watch: false
+    }
+];
