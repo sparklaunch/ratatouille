7da616434031eaaecae101b7739482b50ac18147
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-06-07 18:51:10 +0900

    Rename admin press-release page.

--- src/app/[locale]/admin/main/press-releases/[pressReleaseID]/layout.tsx
+++ src/app/[locale]/admin/main/press-releases/[pressReleaseID]/layout.tsx
@@ -1,7 +1,7 @@
 import AdminHeader from "@/components/adminHeader/AdminHeader";
 import { ReactNode } from "react";
 
-export default async function AdminArticleContentLayout({ children }: { children: ReactNode }) {
+export default async function AdminPressReleaseContentLayout({ children }: { children: ReactNode }) {
     return <>
         <AdminHeader />
         {children}
--- src/app/[locale]/admin/main/press-releases/[pressReleaseID]/page.tsx
+++ src/app/[locale]/admin/main/press-releases/[pressReleaseID]/page.tsx
@@ -1,40 +1,40 @@
 "use client";
 
 import { useRouter } from "@/i18n/routing";
-import defaultArticle, { Article } from "@/types/PressRelease";
+import defaultPressRelease, { PressRelease } from "@/types/PressRelease";
 import formatDate from "@/utilities/formatDate";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import styles from "./style.module.scss";
 
-export default function AdminArticleContentPage() {
-    const { articleID } = useParams() as { articleID: string };
+export default function AdminPressReleaseContentPage() {
+    const { pressReleaseID } = useParams() as { pressReleaseID: string };
     const router = useRouter();
-    const [article, setArticle] = useState<Article>(defaultArticle);
+    const [pressRelease, setPressRelease] = useState<PressRelease>(defaultPressRelease);
     useEffect(() => {
-        const getArticle = async () => {
+        const getPressRelease = async () => {
             try {
-                const response = await fetch(`/api/article?id=${articleID}`);
+                const response = await fetch(`/api/press-release?id=${pressReleaseID}`);
                 if (response.ok) {
-                    const article = await response.json() as Article;
-                    setArticle(article);
+                    const pressRelease = await response.json() as PressRelease;
+                    setPressRelease(pressRelease);
                 }
             } catch (error) {
                 console.error(error);
             }
         };
-        getArticle();
-    }, [articleID]);
+        getPressRelease();
+    }, [pressReleaseID]);
     return <>
         <div className={styles.container}>
             <div className={styles.contentContainer}>
                 <div className={styles.titleContainer}>
-                    <h2 className={styles.title}>{article.title}</h2>
-                    <h3 className={styles.createdDate}>작성일: {formatDate(article.createdAt)}</h3>
+                    <h2 className={styles.title}>{pressRelease.title}</h2>
+                    <h3 className={styles.createdDate}>작성일: {formatDate(pressRelease.createdAt)}</h3>
                 </div>
                 <div className={styles.bodyContainer}>
                     <div dangerouslySetInnerHTML={{
-                        __html: article.content
+                        __html: pressRelease.content
                     }} />
                 </div>
             </div>
