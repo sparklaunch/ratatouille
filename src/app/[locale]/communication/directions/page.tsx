3026d5d730970b865c8ede74d80650d32a841cdc
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-06-07 17:55:00 +0900

    Rename directions page.

--- src/app/[locale]/communication/directions/page.tsx
+++ src/app/[locale]/communication/directions/page.tsx
@@ -7,7 +7,7 @@ import Script from "next/script";
 import Map from "./Map";
 import styles from "./style.module.scss";
 
-export default async function DirectionPage() {
+export default async function DirectionsPage() {
     const t = await getTranslations("directions");
     return <>
         <Script src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NAVER_MAP_API_ID}`} strategy="beforeInteractive" />
@@ -15,18 +15,18 @@ export default async function DirectionPage() {
         <div className={styles.container}>
             <h1 className={styles.header}>{t("communication")}</h1>
             <div className={styles.subheader}>
-                <Link href="/communication/newsletter">
-                    <span>{t("news-letter")}</span>
+                <Link href="/communication/newsletters">
+                    <span>{t("news-letters")}</span>
                 </Link>
-                <Link href="/communication/direction" className={styles.activeSubheader}>
+                <Link href="/communication/directions" className={styles.activeSubheader}>
                     <span>{t("directions")}</span>
                 </Link>
-                <Link href="/communication/inquiry">
-                    <span>{t("inquiry")}</span>
+                <Link href="/communication/inquiries">
+                    <span>{t("inquiries")}</span>
                 </Link>
             </div>
             <div className={styles.addressContainer}>
-                <Image src="/images/Disney.webp" alt={t("photo")} fill className={styles.addressPhoto} />
+                <Image src="/images/Disney.webp" alt={t("house-photo-alternate")} fill className={styles.addressPhoto} />
                 <div className={styles.address}>
                     <div className={styles.addressItem}>
                         <h2>{t("address-title")}</h2>
