0c1beadf1a28bc3222f482186e9b57e920f0d20c
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-06-07 17:47:57 +0900

    Rename newsletters page.

--- src/app/[locale]/communication/newsletters/page.tsx
+++ src/app/[locale]/communication/newsletters/page.tsx
@@ -7,21 +7,21 @@ import { Suspense } from "react";
 import NewsLetterList from "./NewsLetterList";
 import styles from "./style.module.scss";
 
-export default async function NewsLetterPage() {
+export default async function NewsLettersPage() {
     const t = await getTranslations("news-letters");
     return <>
         <Header />
         <div className={styles.container}>
             <h1 className={styles.header}>{t("communication")}</h1>
             <div className={styles.subheader}>
-                <Link href="/communication/newsletter">
-                    <span className={styles.activeSubheader}>{t("news-letter")}</span>
+                <Link href="/communication/newsletters">
+                    <span className={styles.activeSubheader}>{t("news-letters")}</span>
                 </Link>
-                <Link href="/communication/direction">
+                <Link href="/communication/directions">
                     <span>{t("directions")}</span>
                 </Link>
-                <Link href="/communication/inquiry">
-                    <span>{t("inquiry")}</span>
+                <Link href="/communication/inquiries">
+                    <span>{t("inquiries")}</span>
                 </Link>
             </div>
             <Suspense fallback={<Skeleton />}>
