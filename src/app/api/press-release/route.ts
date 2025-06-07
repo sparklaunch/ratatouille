76f31d93ba79fdaf6fbb3cb92ce37ab828c6cae1
Jinwook Kim <redwoodnatlpark@gmail.com>
2025-06-07 18:35:55 +0900

    Rename press-release model.

--- src/app/api/press-release/route.ts
+++ src/app/api/press-release/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "../../../../lib/prisma";
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const id = searchParams.get("id") as string;
-    const pressRelease = await prisma.article.findUnique({
+    const pressRelease = await prisma.pressRelease.findUnique({
         where: {
             id
         }
--- src/app/api/press-releases/route.ts
+++ src/app/api/press-releases/route.ts
@@ -5,14 +5,14 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const page = parseInt(searchParams.get("page") as string);
     const pageSize = 12;
-    const pressReleases = await prisma.article.findMany({
+    const pressReleases = await prisma.pressRelease.findMany({
         orderBy: {
             createdAt: "desc"
         },
         skip: (page - 1) * pageSize,
         take: pageSize
     });
-    const totalCount = await prisma.article.count();
+    const totalCount = await prisma.pressRelease.count();
     return NextResponse.json({
         pressReleases,
         totalCount
